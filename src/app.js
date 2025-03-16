a1fb435ab7c6ecc54b7caf6812ec55309475ba46
Clayton Han-Mitchell <hammer2j2@gmail.com>
2025-03-16 11:44:37 -0700

    no wiring

--- src/app.js
+++ src/app.js
@@ -1,29 +1,4 @@
-// filepath: /Users/mitchhan/git/node/otel/src/app.js
-// const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
-// const { SimpleSpanProcessor } = require('@opentelemetry/sdk-trace-base');
-// const { ConsoleSpanExporter } = require('@opentelemetry/sdk-trace-base');
-// const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-http');
 const express = require('express');
-const { start } = require('@splunk/otel');
-
-// // Set up OpenTelemetry
-// const provider = new NodeTracerProvider();
-// provider.addSpanProcessor(new SimpleSpanProcessor(new ConsoleSpanExporter()));
-// provider.addSpanProcessor(new SimpleSpanProcessor(new OTLPTraceExporter({
-//   url: process.env.OTEL_EXPORTER_OTLP_ENDPOINT,
-//   headers: {
-//     'Content-Type': 'application/json'
-//   }
-// })));
-
-// provider.register();
-
-// Initialize OpenTelemetry with Splunk
-start({
-  serviceName: 'service1-example-nodejs',
-//   metrics: { runtimeMetricsEnabled: true },
-//   profiling: { memoryProfilingEnabled: true }
-});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
